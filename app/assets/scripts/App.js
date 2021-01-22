bc03fc4a1a7cbc0493b9be1ec8fe3671a30d4164
Alexander <alexandertisko@hotmail.com>
2021-01-22 01:14:02 -0300

    beforeExpress

---
+++ app/assets/scripts/App.js
@@ -0,0 +1,7 @@
+import "../styles/style.css";
+import Navbar from "./modules/Navbar";
+import ScrollOut from "scroll-out";
+
+if (module.hot) {
+  module.hot.accept();
+}
---
+++ app/assets/scripts/modules/Navbar.js
@@ -0,0 +1,25 @@
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".menu-links");
+  const navLinks = document.querySelectorAll(".menu-links li");
+  const navLinksA = document.querySelectorAll(".menu-links li a");
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("menu-active");
+    //animate links
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.3s ease forwards ${
+          index / 7 + 0.4
+        }s`;
+        console.log(index / 7);
+      }
+    });
+    //Burger animation
+    burger.classList.toggle("toggle");
+  });
+};
+navSlide();
+
+export default Navbar;
